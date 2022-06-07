a34e1d34f732da519805546c320ecc7b13be3663
ToniFankhauser <toni@ovento.ch>
2022-06-07 14:38:01 +0200

    add dependendy
    dependency website, website_sale, web added

--- ov_wine/__manifest__.py
+++ ov_wine/__manifest__.py
@@ -19,7 +19,7 @@ This addon extends the product-structure with specific wine-attributes.
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'product', 'account'],
+    'depends': ['base', 'sale', 'product', 'account', 'website','website_sale','web'],
 
     # always loaded
     'data': [
